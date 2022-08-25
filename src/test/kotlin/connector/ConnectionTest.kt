458130d36d317e1b54b4bbd910d940aa7ad19b77
mr-meetpatel <pm2410.ldrp@gmail.com>
2022-08-25 11:11:37 +0530

    MP | Testing of Connection Class

--- src/test/kotlin/connector/ConnectionTest.kt
+++ src/test/kotlin/connector/ConnectionTest.kt
@@ -1,5 +1,14 @@
 package connector
 
+import com.google.cloud.firestore.Firestore
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import org.junit.jupiter.api.Assertions.*
 
-internal class ConnectionTest
+internal class ConnectionTest:StringSpec({
+    val db:Firestore = Connection().getConnection()
+    "Return Firestore object which should not null"{
+        db shouldNotBe null
+    }
+})
